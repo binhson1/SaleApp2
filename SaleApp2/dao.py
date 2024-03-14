861b6f73d1a7bd97cf5758b7baf92bad58a8c3c2
binhson1 <101120254+binhson1@users.noreply.github.com>
2024-03-14 11:50:27 +0700

    9:00 PM 13/03/2024 - 11:00 AM 14/03/2024: Main Website with products and image but no side web

---
+++ SaleApp2/dao.py
@@ -0,0 +1,12 @@
+import json
+
+
+def load_categories():
+    with open('data/categories.json', encoding='utf-8') as f:
+        return json.load(f)
+
+
+def load_products():
+    with open('data/products.json', encoding='utf-8') as f:
+        products = json.load(f)
+        return products
---
+++ SaleApp2/index.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+import dao
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    categories = dao.load_categories()
+    products = dao.load_products()
+    return render_template('index.html', categories=categories, products=products)
+    #return "Hi"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
