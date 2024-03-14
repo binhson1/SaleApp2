b465ad5aff5e5a0d72b885f2dcffafefcd8a45b0
binhson1 <101120254+binhson1@users.noreply.github.com>
2024-03-14 22:56:08 +0700

    2:00 PM 13/03/2024 - 11:00 PM 14/03/2024: Update side products-details website and login page

--- SaleApp2/dao.py
+++ SaleApp2/dao.py
@@ -6,7 +6,21 @@ def load_categories():
         return json.load(f)
 
 
-def load_products():
+def load_products(q=None,cate_id=None):
     with open('data/products.json', encoding='utf-8') as f:
         products = json.load(f)
+
+        if q:
+            products = [p for p in products if p['name'].find(q) >= 0]
+        if cate_id:
+            products = [p for p in products if p['category_id'].__eq__(int(cate_id))]
+
         return products
+
+
+def get_product_by_id(product_id):
+    with open('data/products.json', encoding='utf-8') as f:
+        products = json.load(f)
+        for p in products:
+            if p['id'] == product_id:
+                return p
--- SaleApp2/index.py
+++ SaleApp2/index.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from Tools.scripts.make_ctype import method
+
+from flask import Flask, render_template, request, redirect
 
 import dao
 
@@ -7,10 +9,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    categories = dao.load_categories()
-    products = dao.load_products()
-    return render_template('index.html', categories=categories, products=products)
-    #return "Hi"
+    q = request.args.get("q")
+    cate_id = request.args.get("category_id")
+    products = dao.load_products(q, cate_id)
+    return render_template('index.html', products=products)
+    # return "Hi"
+
+
+@app.route('/products/<int:id>')
+def details(id):
+    product = dao.get_product_by_id(product_id=id)
+    return render_template('product-details.html', product=product)
+
+
+@app.context_processor
+def common_attributes():
+    return {
+        'categories': dao.load_categories()
+
+    }
+
+
+@app.route('/login', methods=['get', 'post'])
+def login_my_user():
+    if request.method.__eq__('POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username.__eq__('admin') and password.__eq__('123'):
+            return redirect('/')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
