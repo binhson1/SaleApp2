4940d5d72d936cd4a14af2cbe7de94e644651610
binhson1 <101120254+binhson1@users.noreply.github.com>
2024-04-08 10:07:16 +0700

    2:00 PM 07/04/2024 - 10:00 AM 8/04/2024: Update admin page with product and category view also status view and paging web home website. Pushing data using mysql community

---
+++ SaleApp2/admin.py
@@ -0,0 +1,54 @@
+from flask_admin import Admin, expose
+from __init__ import app, db
+from flask_admin.contrib.sqla import ModelView
+from models import Category, Product
+from flask_admin import BaseView
+from flask_login import logout_user, current_user
+from flask import redirect
+
+
+class MyProductView(ModelView):
+    column_list = ['id', 'name', 'category_id']
+    column_searchable_list = ['id', 'name']
+    column_filters = ['id', 'name', 'price']
+    can_export = True
+    column_labels = {
+        'name': 'Tên sản phẩm',
+        'category_id': 'Danh mục sản phẩm'
+    }
+
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class MyCategoryView(ModelView):
+    column_list = ['id', 'name', 'products']
+
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class StatsView(BaseView):
+    @expose('/')
+    def index(self):
+        return self.render('admin/stats.html')
+
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class LogoutView(BaseView):
+    @expose('/')
+    def index(self):
+        logout_user()
+        return redirect('/admin')
+
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+admin = Admin(app, name='Quản trị trang web bán hàng', template_mode='bootstrap4')
+admin.add_view(MyCategoryView(Category, db.session))
+admin.add_view(MyProductView(Product, db.session))
+admin.add_view(StatsView(name='Thống kê'))
+admin.add_view(LogoutView(name='Đăng xuất'))
---
+++ SaleApp2/models.py
@@ -0,0 +1,61 @@
+import hashlib
+
+from sqlalchemy import Column, String, Float, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from __init__ import db, app
+from flask_login import UserMixin
+
+
+class User(db.Model, UserMixin):
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(100))
+    avatar = Column(String(100))
+    username = Column(String(50), unique=True)
+    password = Column(String(50))
+
+    def __str__(self):
+        return self.name
+
+
+class Category(db.Model):
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+    products = relationship('Product', backref='category', lazy=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(db.Model):
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+    price = Column(Float, default=0)
+    image = Column(String(200), default='https://i.ytimg.com/vi/_BjWq3vHUqM/maxresdefault.jpg')
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+
+    def __str__(self):
+        return self.name
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        # c1 = Category(name='Mobile')
+        # c2 = Category(name='Tablet')
+        # db.session.add_all([c1,  c2])
+        # db.session.commit()
+        # import json
+        #
+        # with open('data/products.json', encoding='utf-8') as f:
+        #     products = json.load(f)
+        #     for p in products:
+        #         prod = Product(**p)
+        #         db.session.add(prod)
+        #
+        # db.session.commit()
+
+        u = User(name='son', username='son',
+                 avatar='https://res.cloudinary.com/dy2dgfaiq/image/upload/v1712508791/image_2024-04-07_235304494_auqfqt.png',
+                 password=str(hashlib.md5('123'.encode('utf-8')).hexdigest()))
+        db.session.add(u)
+        db.session.commit()
