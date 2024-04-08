51bc2028118001838e1fe4e2f53080c7bc239588
binhson1 <101120254+binhson1@users.noreply.github.com>
2024-04-08 10:07:02 +0700

    2:00 PM 07/04/2024 - 10:00 AM 8/04/2024: Update admin page with product and category view also status view and paging web home website. Pushing data using mysql community

--- SaleApp2/dao.py
+++ SaleApp2/dao.py
@@ -1,26 +1,61 @@
 import json
+# from _ast import slice
+from _tracemalloc import start
+import hashlib
+from models import Category, Product, User
+from __init__ import app, login
 
 
 def load_categories():
-    with open('data/categories.json', encoding='utf-8') as f:
-        return json.load(f)
+    return Category.query.all()
+    # with open('data/categories.json', encoding='utf-8') as f:
+    #     return json.load(f)
 
 
-def load_products(q=None,cate_id=None):
-    with open('data/products.json', encoding='utf-8') as f:
-        products = json.load(f)
+def count_product():
+    return int(Product.query.count())
 
-        if q:
-            products = [p for p in products if p['name'].find(q) >= 0]
-        if cate_id:
-            products = [p for p in products if p['category_id'].__eq__(int(cate_id))]
 
-        return products
+def load_products(q=None, cate_id=None, page=None):
+    query = Product.query
+
+    if q:
+        query = query.filter(Product.name.contains(q))
+
+    if cate_id:
+        query = query.filter(Product.category_id.__eq__(cate_id))
+
+    if page:
+        page_size = app.config['PAGE_SIZE']
+        start = (int(page) - 1) * page_size
+        query = query.slice(start, start + page_size)
+    return query.all()
+    # with open('data/products.json', encoding='utf-8') as f:
+    #     products = json.load(f)
+    #
+    #     if q:
+    #         products = [p for p in products if p['name'].find(q) >= 0]
+    #     if cate_id:
+    #         products = [p for p in products if p['category_id'].__eq__(int(cate_id))]
+    #
+    #     return products
+    #
 
 
 def get_product_by_id(product_id):
-    with open('data/products.json', encoding='utf-8') as f:
-        products = json.load(f)
-        for p in products:
-            if p['id'] == product_id:
-                return p
+    return Product.query.get(product_id)
+    # with open('data/products.json', encoding='utf-8') as f:
+    #     products = json.load(f)
+    #     for p in products:
+    #         if p['id'] == product_id:
+    #             return p
+
+
+def get_user_by_id(id):
+    return User.query.get(id)
+
+
+def auth_user(username, password):
+    password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+    return User.query.filter(User.username.__eq__(username.strip()),
+                             User.password.__eq__(password)).first()
--- SaleApp2/index.py
+++ SaleApp2/index.py
@@ -1,18 +1,21 @@
-from Tools.scripts.make_ctype import method
+import math
 
-from flask import Flask, render_template, request, redirect
-
+from flask import render_template, request, redirect
+from flask_login import login_user
 import dao
-
-app = Flask(__name__)
+from admin import admin
+from __init__ import app, login
 
 
 @app.route('/')
 def index():
     q = request.args.get("q")
     cate_id = request.args.get("category_id")
-    products = dao.load_products(q, cate_id)
-    return render_template('index.html', products=products)
+    page = request.args.get('page')
+    products = dao.load_products(q, cate_id=cate_id, page=page)
+    counter = dao.count_product()
+    return render_template('index.html', products=products,
+                           pages=math.ceil(counter / app.config['PAGE_SIZE']))
     # return "Hi"
 
 
@@ -38,6 +41,21 @@ def login_my_user():
         if username.__eq__('admin') and password.__eq__('123'):
             return redirect('/')
     return render_template('login.html')
+
+
+@app.route('/admin-login', methods=['post'])
+def process_admin_login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    u = dao.auth_user(username=username, password=password)
+    if u:
+        login_user(user=u)
+    return redirect('/admin')
+
+
+@login.user_loader
+def load_user(user_id):
+    return dao.get_user_by_id(user_id)
 
 
 if __name__ == '__main__':
